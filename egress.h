96424991e784390a1fc1bd614181cccf43694274
cdkersey <chad.d.kersey@gmail.com>
2013-12-09 16:33:01 -0500

    vector<node> integer egress.

--- egress.h
+++ egress.h
@@ -4,11 +4,13 @@
 #include <chdl/nodeimpl.h>
 #include <chdl/tickable.h>
 
+#include <vector>
 #include <functional>
 
 namespace chdl {
   template <typename T> void EgressFunc(const T& f, node n);
   static void Egress(bool &b, node n);
+  template <typename T> void EgressInt(T &i, const std::vector<node> &v);
   template <typename T, unsigned N> void EgressInt(T &i, bvec<N> bv);
 
   template <typename T> class egress : public tickable {
@@ -31,19 +33,31 @@ static void chdl::Egress(bool &b, chdl::node n) {
   chdl::EgressFunc([&b](bool val){ b = val; }, n);
 }
 
-template <typename T, unsigned N> void chdl::EgressInt(T &x, chdl::bvec<N> bv)
+template <typename T, unsigned N>
+  void chdl::EgressInt(T &x, chdl::bvec<N> bv)
+{
+  using namespace std;
+  using namespace chdl;
+
+  vector<node> v;
+  for (unsigned i = 0; i < N; ++i) v.push_back(bv[i]);
+  EgressInt(x, v);
+}
+
+template <typename T>
+  void chdl::EgressInt(T &x, const std::vector<node> &v)
 {
   using namespace chdl;
   using namespace std;
 
   x = 0;
 
-  for (unsigned i = 0; i < N; ++i)
+  for (unsigned i = 0; i < v.size(); ++i)
     chdl::EgressFunc(
       [i, &x](bool val){
         if (val) x |= (1ull<<i); else x &= ~(1ull<<i);
       },
-      bv[i]
+      v[i]
     );
 }
 #endif
