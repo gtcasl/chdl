164f5a1e291ce204173ef2a78fb8b0d44e087a9f
cdkersey <chad.d.kersey@gmail.com>
2013-12-10 15:37:00 -0500

    Added tmat model to arithmetic example.

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -18,12 +18,15 @@
 
 #include <analysis.h>
 
+#include <tmat.h>
+
 #include "report.h"
 
 using namespace std;
 using namespace chdl;
 
 int main(int argc, char **argv) {
+  #if 0
   // The design
   bvec<4> a, b, c, d, ax;
   bvec<8> v, v2;
@@ -55,12 +58,25 @@ int main(int argc, char **argv) {
   TAP(v); TAP(w); TAP(x); TAP(y); TAP(z);
   TAP(o);
   TAP(a_b_eq);
+  #endif
+
+  bvec<32> next_a, a(Reg(next_a)), next_b, b(Reg(next_b));
+  next_a = a + Lit<32>(0xfedcba98 + 7);
+  next_b = b + Lit<32>(0x76543211 + 5);
+
+  bvec<32> p(a * b);
+
+  TAP(p); TAP(a); TAP(b);
 
   // The simulation (generate .vcd file)
   optimize();
 
+  ofstream netl_file("example2.netl");
+  techmap(netl_file);
+  netl_file.close();
+
   ofstream wave_file("example2.vcd");
-  run(wave_file, 10);
+  run(wave_file, 100000);
 
   ofstream netlist_file("example2.nand");
   print_netlist(netlist_file);
@@ -70,11 +86,25 @@ int main(int argc, char **argv) {
   print_dot(dot_file);
   dot_file.close();
 
-  ofstream netl_file("example2.netl");
-  techmap(netl_file);
-  netl_file.close();
-
   cout << "Critical path length: " << critpath() << endl;
+
+  // Print transition matrices:
+  ofstream mat("example6.tmat");
+  tmat_report(mat);
+
+  ifstream elib("ELIB");
+  while (!!elib) {
+    string type;
+    elib >> type;
+    vector<double> emat;
+    //cout << "Reading for " << type << endl;
+    do {
+      double d;
+      elib >> d;
+      emat.push_back(d);
+    } while (!!elib && elib.peek() != '\n');
+    if (!!elib) tmat_compute(type, emat);
+  }
 
   report();
 
